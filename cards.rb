4e9d8995d448303969c068989ecc4ce6240f61d7
David McVicar <dave.mcvicar@livingsocial.com>
2012-11-17 16:34:00 -0500

    merged all card ruby Classes into Cards module

--- cards.rb
+++ cards.rb
@@ -1,90 +1,253 @@
-class Cards
-
-  attr_reader :cards
-  attr_reader :card_source
-
-  def initialize(card_source, cards=[])
-    @card_source = card_source
-    @cards = cards
-  end
-
-  def shuffle
-    @cards.shuffle!
-  end
-
-  def shuffle_up(num_times=1)
-    num_times.times {shuffle; split}
-    @cards
-  end
-
-  def facing(direction=Card::FACE_DOWN)
-    @cards.each {|c| c.facing(direction)}
-  end
-
-  def split
+module Cards
+  class Card
     #
-    # pick a random spot in the middle third of the deck
-    # and split the deck there
+    # ========================================
+    #                C A R D
+    # ========================================
     #
-    third = num_cards/3
-    split_at = third + rand(third)
-    @cards = @cards.slice(split_at..-1) + @cards.slice(0,split_at)
-  end
+    include Comparable
 
-  def transfer(destination, num_cards, direction=Card::FACE_DOWN)
-    destination.add(remove(num_cards, direction))
-  end
+    CLUBS='C'
+    HEARTS='H'
+    DIAMONDS='D'
+    SPADES='S'
 
-  def deal(how_many=1, direction=Card::FACE_DOWN)
-    #
-    # create a new Cards object using how_many from this set
-    #
-    Cards.new(self, remove(how_many, direction))
-  end
+    CARD_TOP    = ".-------." # 0
 
-  def fold(direction=Card::FACE_DOWN)
-    #
-    # return @cards to the @card_source
-    #
-    transfer(@card_source, num_cards, direction)
-  end
+    SUIT_PATTERNS = {
+      HEARTS => [
+        "|%2s_  _ |", # 1
+        '| ( \/ )|',  # 2
+        '|  \  / |',  # 3
+        "|   \\/%2s|"  # 4
+      ],
+      DIAMONDS => [
+        "|%2s /\\  |",
+        '|  /  \ |',
+        '|  \  / |',
+        "|   \\/%2s|"
+      ],
+      CLUBS => [
+        "|%2s _   |",
+        "|  ( )  |",
+        "| (_x_) |",
+        "|   Y %2s|"
+      ],
+      SPADES => [
+        "|%2s .   |",
+        '|  / \  |',
+        "| (_,_) |",
+        "|   I %2s|"
+      ]
+    }
 
-  def add(cards)
-    @cards += cards
-  end
+    CARD_BOTTOM = "`-------'" # 5
 
-  def remove(how_many, direction)
-    raise "too few cards remaining" if how_many > num_cards
-    @cards.slice!(0, how_many).each {|c| c.facing(direction)}
-  end
+    NUM_PRINT_ROWS=6
 
-  def order
-    @cards.sort!
-  end
+    ACE   = 'A'
+    TWO   = '2'
+    THREE = '3'
+    FOUR  = '4'
+    FIVE  = '5'
+    SIX   = '6'
+    SEVEN = '7'
+    EIGHT = '8'
+    NINE  = '9'
+    TEN   = '10'
+    JACK  = 'J'
+    QUEEN = 'Q'
+    KING  = 'K'
 
-  def num_cards
-    @cards.length
-  end
+    SUITS=[CLUBS, HEARTS, DIAMONDS, SPADES]
+    FACES=[TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE]
 
-  def to_s
-    @cards.map &:to_s
-  end
 
-  def inspect
-    @cards.inspect
-  end
+    attr_reader :face
+    attr_reader :suit
 
-  def [](index)
-    @cards[index]
-  end
+    FACE_DOWN=false
+    FACE_UP=true
+    attr_reader :facing  # FACE_UP, FACE_DOWN
 
-  def print
-    sep = "  "
-    row = ""
-    Card::NUM_PRINT_ROWS.times do |i|
-      puts @cards.map {|card| card.print_row(i)}.join(sep)
+    def initialize(face, suit, direction=FACE_DOWN)
+      @face = face
+      @suit = suit
+      @facing = direction
     end
-    nil
+
+    def facing(direction)
+      @facing = direction
+    end
+
+    def self.for(face_index, suit_index, direction=FACE_DOWN)
+      Card.new(FACES[face_index-2], SUITS[suit_index], direction)
+    end
+
+    def to_s
+      @face + @suit
+    end
+
+    def inspect
+      to_s
+    end
+
+    def print_row(n)
+      case n
+        when 0
+          CARD_TOP
+        when 1,4
+          @facing == FACE_UP ? SUIT_PATTERNS[@suit][n-1] % @face : "| x   x |"
+        when 2,3
+          @facing == FACE_UP ? SUIT_PATTERNS[@suit][n-1] : "|   x   |"
+        when 5
+          CARD_BOTTOM
+      end
+    end
+
+    def print
+      NUM_PRINT_ROWS.times do |i|
+        puts print_row(i)
+      end
+      nil
+    end
+
+    def <=>(anOther)
+      cmp = SUITS.index(@suit) <=> SUITS.index(anOther.suit)
+      cmp.zero? ? FACES.index(@face) <=> FACES.index(anOther.face) : cmp
+    end
+
+    def self.all(direction=FACE_DOWN)
+      a = []
+      SUITS.each do |s|
+        FACES.each do |f|
+          a << Card.new(f, s, direction)
+        end
+      end
+      a
+    end
+  end
+
+
+  class Cards
+    #
+    # ========================================
+    #                C A R D S
+    # ========================================
+    #
+    attr_reader :cards
+    attr_reader :card_source
+
+    def initialize(card_source, cards=[])
+      @card_source = card_source
+      @cards = cards
+    end
+
+    def shuffle
+      @cards.shuffle!
+    end
+
+    def shuffle_up(num_times=1)
+      num_times.times {shuffle; split}
+      @cards
+    end
+
+    def facing(direction=Card::FACE_DOWN)
+      @cards.each {|c| c.facing(direction)}
+    end
+
+    def split
+      #
+      # pick a random spot in the middle third of the deck
+      # and split the deck there
+      #
+      third = num_cards/3
+      split_at = third + rand(third)
+      @cards = @cards.slice(split_at..-1) + @cards.slice(0,split_at)
+    end
+
+    def transfer(destination, num_cards, direction=Card::FACE_DOWN)
+      destination.add(remove(num_cards, direction))
+    end
+
+    def deal(how_many=1, direction=Card::FACE_DOWN)
+      #
+      # create a new Cards object using how_many from this set
+      #
+      Cards.new(self, remove(how_many, direction))
+    end
+
+    def fold(direction=Card::FACE_DOWN)
+      #
+      # return @cards to the @card_source
+      #
+      transfer(@card_source, num_cards, direction)
+    end
+
+    def add(cards)
+      @cards += cards
+    end
+
+    def remove(how_many, direction)
+      raise "too few cards remaining" if how_many > num_cards
+      @cards.slice!(0, how_many).each {|c| c.facing(direction)}
+    end
+
+    def order
+      @cards.sort!
+    end
+
+    def num_cards
+      @cards.length
+    end
+
+    def to_s
+      @cards.map &:to_s
+    end
+
+    def inspect
+      @cards.inspect
+    end
+
+    def [](index)
+      @cards[index]
+    end
+
+    def print
+      sep = "  "
+      row = ""
+      Card::NUM_PRINT_ROWS.times do |i|
+        puts @cards.map {|card| card.print_row(i)}.join(sep)
+      end
+      nil
+    end
+  end
+
+  class Deck < Cards
+    #
+    # ========================================
+    #               D E C K
+    # ========================================
+    #
+    def initialize(num_decks=1, direction=Card::FACE_DOWN)
+      cards = []
+      num_decks.times {cards += Card.all(direction)}
+      super(nil, cards)
+    end
+
+    def deal_hands(num_hands, how_many_cards, direction=Card::FACE_DOWN)
+      #
+      # create Hands from this deck, dealing one card out at a time to each Hand
+      #
+      hands=[]
+      num_hands.times {hands << deal(1, direction)}
+      (how_many_cards-1).times do
+        hands.each do |hand|
+          transfer(hand, 1, direction)
+        end
+      end
+      hands
+    end
   end
 
 end
